caef233e1c4faaad00c89bb8b90cb37abdb4fd12
bsevansunc <bsevans.unc@gmail.com>
2014-05-06 13:12:38 -0400

    Load environmental data
    Adding scripts that load these data for running maxent model and the
    RStudio project file

---
+++ load_env.R
@@ -0,0 +1,33 @@
+##############################################
+# Load environment ascii files
+##############################################
+# The following is one big function that:
+# 1. Sources the asc raster files
+# 2. Combines rasters into a raster stack
+# 3. Assigns values of all rasters into memory
+# 4. Defines the projection of the rasters
+
+
+load.env = function(){
+# Find the raster data (searches for and ID's all files that end in ".asc":
+
+raster_data <- list.files('C:/Users/Brian/Desktop/rubl_2_14/lc_asc',
+                          pattern='\\.asc$', full=T)    
+
+# Create a raster stack of raster layers:
+
+env.stack = stack(raster_data)
+
+# Add raster stack values to memory:
+
+values(env.stack) = getValues(env.stack)
+
+# Add projection information to the raster stack:
+
+newproj = CRS('+proj=longlat +datum=WGS84')
+
+projection(env.stack) = newproj
+
+return(env.stack)
+
+}
---
+++ load_swd.R
@@ -0,0 +1,84 @@
+##############################################
+# Load swd files
+##############################################
+# The following is one big function that:
+# 1. Sources the samples with data (SWD) files
+# 2. Formats them for dismo-maxent
+# 3. Makes a presence-absence column (pa)
+# 4. Assembles as a list of data frames
+
+load.swd = function(){
+# Find the SWD data (searches for and ID's all files that end in ".csv":
+
+swd_data <- list.files('C:/Users/Brian/Desktop/rubl_2_14/swd',
+                          pattern='\\.csv$', full=T)  
+
+# Add data to memory:
+
+swd.list = lapply(swd_data, read.csv)
+
+# Assign names to each of the files:
+
+pts.all = swd.list[[1]]
+pts.bg = swd.list[[2]]
+pts.bz = swd.list[[3]]
+pts.eb = swd.list[[4]]
+
+#-------------------------------------------------------------------------------
+# Prepare swd files for model running
+#-------------------------------------------------------------------------------
+
+# The following function:
+# 1) Subsets swd file to a given flock size
+# 2) Replaces the sp column (flock size) with 1's or 0's
+# 3) Adds a kfold partition
+
+swd.prep = function(swd.file, flock.size){
+  swd = swd.file[swd.file$sp == flock.size,] # Subset by flock
+  swd$sp = if (flock.size == 'bg') rep(0, length(swd$sp))
+  else (rep(1, length(swd$sp))) # Adds counts
+  swd$k = kfold(swd, k=5) # Adds kfold partition ID
+  swd
+}
+
+# SWD function: Binds positive counts (for a flock of a given size)
+# and the background files
+
+swd.fun = function(swd.file, flock.size){
+  swd1 = swd.prep(swd.file, flock.size)
+  swd0 = swd.prep(pts.bg, 'bg')
+  rbind(swd0, swd1)
+}
+
+
+# Run to create swd files:
+
+swd.lf.bz = swd.fun(pts.bz,'lf')
+swd.lf.eb = swd.fun(pts.eb,'lf')
+swd.lf.all = swd.fun(pts.all,'lf')
+
+swd.sf.bz = swd.fun(pts.bz,'sf')
+swd.sf.eb = swd.fun(pts.eb,'sf')
+swd.sf.all = swd.fun(pts.all,'sf')
+
+swd.ind.bz = swd.fun(pts.bz,'ind')
+swd.ind.eb = swd.fun(pts.eb,'ind')
+swd.ind.all = swd.fun(pts.all,'ind')
+
+# Convert to a list:
+
+swd.list = list(swd.lf.bz, swd.lf.eb, swd.lf.all,
+                  swd.sf.bz,swd.sf.eb,swd.sf.all,
+                  swd.ind.bz, swd.ind.eb, swd.ind.all)
+
+# Add names to the list items:
+
+list.names = c('swd.lf.bz','swd.lf.eb','swd.lf.all',
+              'swd.sf.bz','swd.sf.eb','swd.sf.all',
+              'swd.ind.bz','swd.ind.eb','swd.ind.all')
+
+names(swd.list) = list.names
+
+# Return swd.list
+swd.list
+}
