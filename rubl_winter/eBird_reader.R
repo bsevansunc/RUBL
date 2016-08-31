0a3e3c17e2ab72e77b60d91a37474697964712b2
bsevansunc <bsevans.unc@gmail.com>
2016-08-31 15:30:54 -0400

    script for reading big eBird

--- rubl_winter/eBird_reader.R
+++ rubl_winter/eBird_reader.R
@@ -1,8 +1,8 @@
+library(stringr) ; library(dplyr) ; library(lubridate)
 
-
-library(stringr) ; library(dplyr)
-
-
+#---------------------------------------------------------------------------------------------------*
+# ---- FUNCTION TO READ EBIRD DATA ----
+#---------------------------------------------------------------------------------------------------*
 
 
 splitReader <- function(pathToFile, rowSkip, maxRows){
@@ -35,28 +35,101 @@ splitReader <- function(pathToFile, rowSkip, maxRows){
   return(data)
 }
 
-
-readEBird <- function(pathToFile, rowsPerRead){
-  colNames <- read.delim(
-    pathToFile, skip = 0, nrows = 1,
-    header = TRUE) %>% names 
-  # Get the total number of rows in the file:
-  tRows <- R.utils::countLines(pathToFile)[1]
-  # Get the number of records per set:
-  nSets <- plyr::round_any(tRows/rowsPerRead, 1, f = ceiling)
-  outList <- vector('list', length = nSets)
-  for(i in 1:nSets){
-    if(i == 1){
-      rowsToSkip <- 1
-    } else {
-      rowsToSkip <- (i-1) * rowsPerRead
-    }
-    outList[[i]] <- splitReader(pathToFile, rowsToSkip, rowsPerRead)
-  }
-  bind_rows(outList) %>%
-    distinct
-}
+#---------------------------------------------------------------------------------------------------*
+# ---- GET RUSTY OBSERVATIONS ----
+#---------------------------------------------------------------------------------------------------*
 
 pathToFile <- 'C:/Users/Brian/Desktop/ebd_rusbla_relMay-2016/ebd_rusbla_relMay-2016.txt'
 
-rubl1 <- readEBird(pathToFile, 100000)
+# Get vector of column names:
+
+colNames <- read.delim(
+  pathToFile, skip = 0, nrows = 1,
+  header = TRUE) %>% names 
+
+# Get the total number of rows in the file:
+
+tRows <- R.utils::countLines(pathToFile)[1]
+
+# Get the number of records per set:
+
+rowsPerRead <- 1E7
+
+nSets <- plyr::round_any(tRows/rowsPerRead, 1, f = ceiling)
+
+# Read in tRows at a time 
+
+outList <- vector('list', length = nSets)
+
+for(i in 1:nSets){
+  if(i == 1){
+    rowsToSkip <- 1
+  } else {
+    rowsToSkip <- (i-1) * rowsPerRead
+  }
+  outList[[i]] <- splitReader(pathToFile, rowsToSkip, rowsPerRead)
+}
+
+# List to data frame:
+
+rubl1 <- bind_rows(outList) %>%
+  distinct
+
+
+#---------------------------------------------------------------------------------------------------*
+# ---- GET SAMPLING EVENTS ----
+#---------------------------------------------------------------------------------------------------*
+
+
+pathToFile <- 'C:/Users/Brian/Desktop/rubl_summer_2016/ebd_US_200601_201605_relMay-2016/ebd_US_200601_201605_relMay-2016.txt'
+
+# Get vector of column names:
+
+colNames <- read.delim(
+  pathToFile, skip = 0, nrows = 1,
+  header = TRUE) %>% names 
+
+# Get the total number of rows in the file:
+
+tRows <- R.utils::countLines(pathToFile)[1]
+
+# Get the number of iterations:
+
+rowsPerRead <- 1E7
+
+nSets <- plyr::round_any(tRows/rowsPerRead, 1, f = ceiling)
+
+
+# Make progress bar:
+
+outList <- vector('list', length = nSets)
+
+for(i in 1:nSets){
+  if(i == 1){
+    rowsToSkip <- 1
+  } else {
+    rowsToSkip <- (i-1) * rowsPerRead
+  }
+  outList[[i]] <- splitReader(pathToFile, rowsToSkip, rowsPerRead) %>%
+    select(-c(sp, count)) %>%
+    distinct %>%
+    mutate(date = as.Date(date)) %>%
+    filter(month(date) %in% 1:5) %>%
+    distinct
+}
+
+# List to data frame:
+
+samplingEvents <- bind_rows(outList) %>%
+  distinct
+
+saveRDS(samplingEvents, 'samplingEvents.RDS')
+
+
+  splitReader(pathToFile, 1, 1E7) %>%
+  select(-c(sp, count)) %>%
+    distinct %>%
+  mutate(date = as.Date(date)) %>%
+  filter(month(date) %in% 1:5) %>%
+    distinct
+
